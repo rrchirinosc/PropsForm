a8c45602fe06c1ac0d3c445ea782b465d0c07c96
rrchirinosc <rrchirinosc@gmail.com>
2019-04-09 12:30:40 +0200

    Changed Usage description

--- Query.cpp
+++ Query.cpp
@@ -2,13 +2,12 @@
 //			A Windows Forms application that connects to an SQL Server via ODBC and allows to execute and visualize query results.
 //
 // Usage:	
-//			Since this is a test program, in its first version, it has some usability restrictions.
-//			The program requires an ODBC system Data Server Name (DSN) properly installed in the machine hosting the SQL database.
-//			There is support for both 32 and 64bit DSNs depending on what platform the program has been compiled for. As by default 
-//			it is compiled for Win32, it can connect only via 32bit DSNs.
-//			Upon starting, the program queries for a DSN and indicates whether or not the connection succeeded.
-//			Once connected, queries can be executed at the prompt.
-//			Program also accept the DSN from the command-line.
+//					
+//			Once started click the Connect button to launch the ODBC Select Data Source which can be either local or system installed.
+//			If the connecton succeedes, the UI shows the server we are connected to and queries can be executed.
+//			To start, select an existing database in the connected server by doing 'use DBname' and upon success 'select' statements 
+//			can be executed against it.
+//
 //
 // Modules:	
 //			Query, main application single form in charge to connect, execute queries and show result sets
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PropsForm
-A Windows Forms application that connects to an SQL Server via ODBC and allows to execute and visualize query results.
+A Windows Forms application that connects to an SQL Server via ODBC and allows to execute 
+and visualize query results.
+
+Once started click the Connect button to launch the ODBC Select Data Source which can be either 
+local or system installed.
+
+If the connecton succeedes, the UI shows the server we are connected to and queries can be executed.
+
+To start, select an existing database in the connected server by doing 'use DBname' and upon success 
+'select' statements can be executed against it.
